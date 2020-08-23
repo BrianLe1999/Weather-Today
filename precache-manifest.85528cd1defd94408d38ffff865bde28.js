155f10da5c142c3cf5730c82ee30a4ea88303325
BrianLe1999 <minhnguyenlesacramento@gmail.com>
2020-08-22 18:33:34 -0700

    Updates

---
+++ precache-manifest.85528cd1defd94408d38ffff865bde28.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffca9bdabc52859d1a943bb73edad13",
+    "url": "/weather-today/index.html"
+  },
+  {
+    "revision": "b954e22e238ab79401e6",
+    "url": "/weather-today/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "27d7bfe924464d96b5f4",
+    "url": "/weather-today/static/css/main.9aade2a5.chunk.css"
+  },
+  {
+    "revision": "b954e22e238ab79401e6",
+    "url": "/weather-today/static/js/2.bbac7981.chunk.js"
+  },
+  {
+    "revision": "af0281fc0ad21814e4274b397a5b4e20",
+    "url": "/weather-today/static/js/2.bbac7981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d7bfe924464d96b5f4",
+    "url": "/weather-today/static/js/main.7da59a33.chunk.js"
+  },
+  {
+    "revision": "1a51a1621110b2dd56af",
+    "url": "/weather-today/static/js/runtime-main.6e8ec249.js"
+  },
+  {
+    "revision": "20b8232f129f758e2445a2a55ec2390c",
+    "url": "/weather-today/static/media/broken_clouds_day.20b8232f.svg"
+  },
+  {
+    "revision": "588d93dc94d344b34742dbe28fa16cfd",
+    "url": "/weather-today/static/media/broken_clouds_day.98a6c824.jpg"
+  },
+  {
+    "revision": "5de90dbb5cda6bea6e24d198c2b40c9f",
+    "url": "/weather-today/static/media/broken_clouds_night.5de90dbb.svg"
+  },
+  {
+    "revision": "96c07812a010eeda269ab733b5b4c1d6",
+    "url": "/weather-today/static/media/broken_clouds_night.8c28c655.jpg"
+  },
+  {
+    "revision": "ab76c0b1e7693f50bd709e78f492ab3e",
+    "url": "/weather-today/static/media/clear_sky_day.ab76c0b1.svg"
+  },
+  {
+    "revision": "909c238ebf08af67706e5c2b97de626a",
+    "url": "/weather-today/static/media/clear_sky_day1.9833e8b8.jpg"
+  },
+  {
+    "revision": "65cd6c2fcd41cdf03020657e96a66655",
+    "url": "/weather-today/static/media/clear_sky_night.65cd6c2f.svg"
+  },
+  {
+    "revision": "a1ce555c7fac205e829b2c7d718b1403",
+    "url": "/weather-today/static/media/clear_sky_night1.6f54ee3d.jpg"
+  },
+  {
+    "revision": "27615ed94d42084de0daf44b443d3d55",
+    "url": "/weather-today/static/media/few_clouds_day.27615ed9.svg"
+  },
+  {
+    "revision": "d6b0e6450bccceef68ec83ea33c5d510",
+    "url": "/weather-today/static/media/few_clouds_day.e3884f9b.jpg"
+  },
+  {
+    "revision": "7e55e357e58a528bdc7ed9bb67a29aed",
+    "url": "/weather-today/static/media/few_clouds_night.2c18cb83.jpg"
+  },
+  {
+    "revision": "85832d4a712487f3866b8f1e6b242d42",
+    "url": "/weather-today/static/media/few_clouds_night.85832d4a.svg"
+  },
+  {
+    "revision": "6577e2c183071697c200cd6f470d23af",
+    "url": "/weather-today/static/media/mist.6577e2c1.svg"
+  },
+  {
+    "revision": "bf05938627a6196f179adcd3400bf095",
+    "url": "/weather-today/static/media/mist_day.6d6cfd51.jpg"
+  },
+  {
+    "revision": "c7f231127bb987087dcfd6844341eae5",
+    "url": "/weather-today/static/media/mist_night.cd3e9a29.jpg"
+  },
+  {
+    "revision": "3dbe8cd423d8849986f1d5689bd5a417",
+    "url": "/weather-today/static/media/rain_day.56d307f6.jpg"
+  },
+  {
+    "revision": "ca8c463ab32da0549ea98122f1f8f8fa",
+    "url": "/weather-today/static/media/rain_day.ca8c463a.svg"
+  },
+  {
+    "revision": "02502996e8001ebd8fd1f6079b5d1d23",
+    "url": "/weather-today/static/media/rain_night.8e0e8483.jpg"
+  },
+  {
+    "revision": "9e6d84a7a6b0d33b8db4a45650257bd5",
+    "url": "/weather-today/static/media/rain_night.9e6d84a7.svg"
+  },
+  {
+    "revision": "ba347d37d650fbe32de7b89a8e65ab77",
+    "url": "/weather-today/static/media/scattered_clouds_day.ba347d37.svg"
+  },
+  {
+    "revision": "33d0af598c5362f4fb783e330cdae6dd",
+    "url": "/weather-today/static/media/scattered_clouds_day1.8cfdb072.jpg"
+  },
+  {
+    "revision": "318df5390534c61dbd61be58bbf9b295",
+    "url": "/weather-today/static/media/scattered_clouds_night.318df539.svg"
+  },
+  {
+    "revision": "87e08f53775318a812e9892b83a65976",
+    "url": "/weather-today/static/media/scattered_clouds_night.d4253795.jpg"
+  },
+  {
+    "revision": "c37b23d3568857992dd32228f6a79571",
+    "url": "/weather-today/static/media/shower_rain_day.c37b23d3.svg"
+  },
+  {
+    "revision": "832ca43cc3438aa9e0924e57c6263fb7",
+    "url": "/weather-today/static/media/shower_rain_day.ef420781.jpg"
+  },
+  {
+    "revision": "4af1168267bb00a3a9e83487f0bb75af",
+    "url": "/weather-today/static/media/shower_rain_night.22533fa6.jpg"
+  },
+  {
+    "revision": "929c5d926b0cab20b84a4ec163e05d22",
+    "url": "/weather-today/static/media/shower_rain_night.929c5d92.svg"
+  },
+  {
+    "revision": "8ff8054677a60a8889bc61950e78bfef",
+    "url": "/weather-today/static/media/snow_day.573d2ee6.jpg"
+  },
+  {
+    "revision": "c1655029bc3d14c917288bc7b3b2b482",
+    "url": "/weather-today/static/media/snow_day.c1655029.svg"
+  },
+  {
+    "revision": "4dbdf7cd21c10de2a20c05a02a21bd75",
+    "url": "/weather-today/static/media/snow_night.03c14196.jpg"
+  },
+  {
+    "revision": "f90c69e8b959273e6302d040ca628cc4",
+    "url": "/weather-today/static/media/snow_night.f90c69e8.svg"
+  },
+  {
+    "revision": "7b8bd4e3934f34893863b325e1268de2",
+    "url": "/weather-today/static/media/thunderstorm.7b8bd4e3.svg"
+  },
+  {
+    "revision": "779e7b285df34f648a31b6071efcdcd2",
+    "url": "/weather-today/static/media/thunderstorm_day.b606a833.jpg"
+  },
+  {
+    "revision": "1980d2f080d050398b745e60a605d4d7",
+    "url": "/weather-today/static/media/thunderstorm_night.531dc6a9.jpg"
+  }
+]);
